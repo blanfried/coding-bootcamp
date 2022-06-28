879c153c9cae5b024120f7cbd81b2b4c4113e184
Baer Lanfried <baerlanfried@Baers-MacBook-Pro.local>
2022-06-28 21:30:02 +0100

    initial commit

---
+++ 06-Stu_HTML/Unsolved/javascript.js
@@ -0,0 +1,11 @@
+
+ document.getElementById("#faded").addEventListener("click", function(){
+
+    document.getElementById("#picture").style.opacity = "0.1";
+
+ });
+
+    document.getElementById("#reset").addEventListener("click", function(){
+
+        document.getElementById("#picture").style.opacity = "1";
+ });
