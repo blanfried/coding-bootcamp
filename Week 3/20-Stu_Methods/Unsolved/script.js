6021aad769383681d8627a7a01fc4878190ca00d
Baer Lanfried <blanfried@yahoo.com>
2022-07-14 06:28:11 +0100

    commit

--- Week 3/20-Stu_Methods/Unsolved/script.js
+++ Week 3/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,11 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+constellations.sort();
+planets.sort();
+
+console.log(constellations);
+console.log(planets);
+console.log(star);
+
+constellations.unshift("Canis Major")
