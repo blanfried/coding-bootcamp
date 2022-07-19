13ce1439c491d8cc3526bd4ab3f663d97cb02736
Baer Lanfried <blanfried@yahoo.com>
2022-07-19 17:03:07 +0100

    commit

---
+++ Week 4/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+// it will log whatever is in the window
+console.log(window);
+
+// TODO: What will the following line of code log?
+// it will log whatever is in the document in the window
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// it wll log a specific element within the document
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// this will log whatever is written in the head of the document
+console.log(document.head);
+
