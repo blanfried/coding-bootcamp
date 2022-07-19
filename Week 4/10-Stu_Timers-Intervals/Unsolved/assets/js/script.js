ac466509edc20fc1e62689f05cc7100edafd6624
Baer Lanfried <blanfried@yahoo.com>
2022-07-19 18:36:33 +0100

    commit

--- Week 4/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ Week 4/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -7,15 +7,13 @@ var words = message.split(' ');
 
 function countdown() {
   var timeLeft = 5;
-
+}
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    //
-    // YOUR CODE HERE
-    //
-    setInterval (countdown(1000))
-  });
-}
+
+  
+  })
+
 
 // Displays the message one word at a time
 function displayMessage() {
