c7b4fc6885602cf45a223560812c7daee056faee
Baer Lanfried <blanfried@yahoo.com>
2022-07-25 21:29:47 +0100

    commit

--- Week 5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ Week 5/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,5 +2,8 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
-
+rootEl.children("ul").children().css('background-color', 'white');
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl.children("ul").eq(2).children("li").eq(0).text("O");
+rootEl.children("ul").eq(0).children("li").eq(0).text("O");
+
