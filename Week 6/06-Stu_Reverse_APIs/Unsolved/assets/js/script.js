0cf5204dafbafcdb1f34b43015d15e80ec007683
Baer Lanfried <blanfried@yahoo.com>
2022-08-01 19:59:37 +0100

    commit

--- Week 6/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
+++ Week 6/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
@@ -2,6 +2,7 @@ var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 
 // JQuery AJAX
 // TODO: Comment on how AJAX returns an API call
+// AJAX needs a third-party library (in this case JQUERY) to work. It returns the data in an array
 $.ajax({
   url: requestUrl,
   method: 'GET',
@@ -12,6 +13,7 @@ $.ajax({
 
 // Browser Fetch Method
 // TODO: Comment on how Fetch returns an API call
+// Fetch returns an API call in an array as well but does it by pulling straight from the browser
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -23,6 +25,7 @@ fetch(requestUrl)
 
 // Browser XMLHttpRequest
 // TODO: Comment on how XMLHttpRequest returns an API call
+// XMLHttpRequest gets the data straight from the browser and puts it in a rough format (ie not an array (maybe a string?))
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -34,3 +37,4 @@ xhr.open('GET', requestUrl);
 xhr.send();
 
 // TODO: Comment on the differences on the format of the data that was returned
+// I did this within the comments above
