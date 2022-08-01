3340a95a0c9e20c0a0887be62a4cfc769247480b
Baer Lanfried <blanfried@yahoo.com>
2022-08-01 19:51:45 +0100

    commit

--- Week 6/Notes/session01/script.js
+++ Week 6/Notes/session01/script.js
@@ -1,6 +1,6 @@
 function averageNumber(arr) {
       let sum = 0
-      for (i = 0; i < arr.length; i++){
+      for (let i = 0; i < arr.length; i++){
       sum += arr[i]
       console.log(sum / arr.length)
       }
