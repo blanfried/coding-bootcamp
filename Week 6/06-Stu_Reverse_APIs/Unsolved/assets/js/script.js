47e2506e7475c9d923b026e6d5a98f5a580ac661
Baer Lanfried <blanfried@yahoo.com>
2022-08-01 20:07:13 +0100

    commit

--- Week 6/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
+++ Week 6/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
@@ -2,7 +2,7 @@ var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 
 // JQuery AJAX
 // TODO: Comment on how AJAX returns an API call
-// AJAX needs a third-party library (in this case JQUERY) to work. It returns the data in an array
+// AJAX needs a third-party library (in this case JQUERY) to work. It returns the data in an array. AJAX is also compatible with all browser, including internet explorer (which fetch is not compatible with).
 $.ajax({
   url: requestUrl,
   method: 'GET',
@@ -13,7 +13,7 @@ $.ajax({
 
 // Browser Fetch Method
 // TODO: Comment on how Fetch returns an API call
-// Fetch returns an API call in an array as well but does it by pulling straight from the browser
+// Fetch returns an API call in an array as well but does it by pulling straight from the browser. It also provides a promise of the data soon to be obtained, which is an advancement from XMLHttpRequest. Although, Fetch is not compatible with internet explorer, but it is compatible with all recent browsers. 
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -25,7 +25,7 @@ fetch(requestUrl)
 
 // Browser XMLHttpRequest
 // TODO: Comment on how XMLHttpRequest returns an API call
-// XMLHttpRequest gets the data straight from the browser and puts it in a rough format (ie not an array (maybe a string?))
+// XMLHttpRequest gets XML data straight from the browser and puts it in XML format
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
