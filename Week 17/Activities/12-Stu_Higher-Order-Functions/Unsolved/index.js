7ae29d03d95edafe7b96dda38a2e9a5d469972b4
Baer Lanfried <blanfried@yahoo.com>
2022-10-24 10:50:51 -0700

    commit

--- Week 17/Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
+++ Week 17/Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
@@ -2,10 +2,12 @@ const grades = [56, 78, 99, 85];
 
 // TODO: Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
-    
-}
+      if (index === array.length - 1) {
+            return (accumulator + currentValue) / array.length;
+          }
+        }
 
 // TODO: Pass the 'findAverage' into the reduce method.
-let gradeAverage = grades.reduce();
+let gradeAverage = grades.reduce(findAverage);
 
 console.log(gradeAverage);
