d86004ddec1bf6708b5a636d385bfc69da40a98e
Baer Lanfried <blanfried@yahoo.com>
2022-11-03 14:05:28 -0700

    commit

--- Week_20/Activities/04-Stu_Render-Element/Unsolved/src/components/HelloDiv.js
+++ Week_20/Activities/04-Stu_Render-Element/Unsolved/src/components/HelloDiv.js
@@ -1,8 +1,19 @@
 // TODO: Fix the return statement so that it renders an `h1` heading tag that introduces the creator
 function HelloDiv() {
-  return "Hello";
+  return (
+    <div>
+      <h1>Hey guys! My name is {name}!</h1>
+      <h2>Things I like!</h2>
+      <ul>
+        <li>Peas</li>
+        <li>Carrots</li>
+        <li>Love</li>
+      </ul>
+    </div>
+  );
 }
 
 // TODO: Fix the export statement
+const name = "Baer"
 // Export statements are ES6 syntax that tell Javascript what "thing" we want to make available outside of this file
-export HelloDiv;
+export default HelloDiv;
